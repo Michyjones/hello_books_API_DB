831d53a4753733f254ec8522a111b66899afd43d
Michyjones <mykembuguah11@gmail.com>
2018-04-27 11:40:50 +0300

    add security revoerable to be true

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
+    app.config['SECURITY_RECOVERABLE'] = True
     db.init_app(app)
 
     return app
