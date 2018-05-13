68c3ff2f1936323294995a88854239e6dfd1fd37
Michyjones <mykembuguah11@gmail.com>
2018-05-13 12:50:37 +0300

    Update blueprint on run

--- run.py
+++ run.py
@@ -1,12 +1,7 @@
 import os
 from app import create_app
-from app.users.views import user
-from app.books.views import book
 
 app = create_app(config_name="development")
-
-app.register_blueprint(user)
-app.register_blueprint(book)
 
 
 if __name__ == '__main__':
