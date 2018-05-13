d70c3e48752e0478d69a808f8e7bc2251a2ccb6e
Michyjones <mykembuguah11@gmail.com>
2018-05-13 12:31:31 +0300

    Restructure the import order

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,18 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
-    app.config['SECURITY_RECOVERABLE'] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+# app = Flask(__name__)
+# app.config.from_object(app_config[config_name])
+# app.config['SECURITY_RECOVERABLE'] = True
+# db.init_app(app)
+    from app.users.views import user
+    app.register_blueprint(user)
+
+    from app.books.views import book
+    app.register_blueprint(book)
 
     return app
