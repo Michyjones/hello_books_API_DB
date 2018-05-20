50cd5973ae4f0925612dd671ec05a18e54c08157
Mike Mbugua <mykembuguah11@gmail.com>
2018-05-20 11:57:21 +0300

    Add 404 error handler

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from config import app_config
 
@@ -11,14 +11,20 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-# app = Flask(__name__)
-# app.config.from_object(app_config[config_name])
-# app.config['SECURITY_RECOVERABLE'] = True
-# db.init_app(app)
+
     from app.users.views import user
     app.register_blueprint(user)
 
     from app.books.views import book
     app.register_blueprint(book)
+
+    @app.errorhandler(404)
+    def invalidendpoints(error=None):
+        """Method for invalid endpoints."""
+        message = {
+            'message': 'The url entered is Invalid!!!',
+            'URL': 'Not found : ' + request.url
+        }
+        return jsonify(message), 404
 
     return app
