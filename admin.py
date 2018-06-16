3c004b938a367147b6dfb2273ae0453d001263d4
Michyjones <mykembuguah11@gmail.com>
2018-06-16 13:20:07 +0300

    [Feature] Update admin file

--- admin.py
+++ admin.py
@@ -6,17 +6,34 @@ from app import create_app
 app = create_app(config_name="development")
 app.app_context().push()
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 7:
     print("Run this command- python admin.py email password")
     exit()
 
-email = sys.argv[1]
-password = sys.argv[2]
+first_name = sys.argv[1]
+last_name = sys.argv[2]
+address = sys.argv[3]
+email = sys.argv[4]
+password = sys.argv[5]
+confirm_password = sys.argv[6]
 
-new_admin = User(email=email.strip(), password=password, IsAdmin=True)
+person = User.query.filter_by(email=email)
+if person:
+    print("User already exist")
 
-try:
-    new_admin.save()
-    print("Admin create successfully!")
-except IntegrityError:
-    print()
+if password == confirm_password:
+
+    new_admin = User(first_name=first_name.strip(),
+                     last_name=last_name.strip(),
+                     address=address,
+                     email=email.strip(),
+                     password=password,
+                     IsAdmin=True)
+
+    try:
+        new_admin.save()
+        print("Admin create successfully!")
+    except IntegrityError:
+        print()
+else:
+    print("Password mismatch")
